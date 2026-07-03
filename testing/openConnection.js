6e885ff4cbbc70b29b8f35489c0998be94f95d0e
ILikeJasmineRixe <ilikejasminerice@gmail.com>
2026-07-02 21:27:17 -0500

    varInt is my fucking nemesis

--- testing/openConnection.js
+++ testing/openConnection.js
@@ -1,7 +1,9 @@
 const net = require('net');
+const varint = require('varint');
 const dotenv = require('dotenv').config(
     // { quiet: true }
 );
+
 const PORT = 25565;
 const HOST = process.env.HOST;
 const hostnameBytes = Buffer.from(HOST, 'utf8');
@@ -18,5 +20,5 @@ socket.setTimeout(5000);
 socket.on('connect', () => {
   console.log('connection opened');
   // write the buffer to the websocket via socket.write(openingPacket) once our packet's buffer has been fully defined.
-
+  
 });
--- testing/varint.js
+++ testing/varint.js
@@ -1,8 +1,13 @@
-function writeVarInt(value) {
+// ok so i dont want to deal with all this rn because im so so confused.
+// UPDATE: for now gonna use the varInt lib instead.
+
+function encodeVarInt(value) {
   const bytes = [];
+  value = value >>> 0; // craxy fax; the >>> (unsigned righr shift) operator turns js integers into 32bit instead of 64bit????
+
 }
 
-function readVarInt() {
+function decodeVarInt() {
 }
 
-module.exports = { writeVarInt, readVarInt };
+module.exports = { encodeVarInt, decodeVarInt };
